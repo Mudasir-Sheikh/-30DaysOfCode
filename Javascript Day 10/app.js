9ade6ac433e9768929724ff18476895ca29d61fc
Mudasir-Sheikh <mudasirsheeikh@gmail.com>
2024-04-01 02:16:33 +0500

    Day 10

--- Javascript Day 10/app.js
+++ Javascript Day 10/app.js
@@ -71,7 +71,87 @@ console.log(stringsObject); //assigns index as a key to the spread values.
 
 
 
+//Rest Function --> Allows a Function to take indefinite number of arguments and bundle them into an array.
 
+function mysum(...args){       //it takes indefinite arguments and convert/bundle them into an array.
+    for (let i=0; i<args.length; i++){
+        console.log("You gave us : ",args[i]); //that's why we are able to use array methods
+    }
+}
+mysum(1,2,3,4);
+
+
+function min(a,b,c,d){
+    console.log(arguments); //every function has a collection object which collects their data known as arguments
+                            //but we can not apply any method on them thats why there built-in auto collection is 
+                            // not useful at all thats why we use rest method to covert the collection into an "Array"  
+}
+min(1,2,3,4);
+
+
+//Creating Sum Function using Rest method;
+function my_sum(...args){
+    return args.reduce( (sum , ele) => {
+        return sum+ele;
+    })
+}
+console.log(my_sum(1,2,3,4,5));
+
+
+//Creating math.max function;
+function math_max(...args){
+    return args.reduce((max , current) => {
+        if( current > max){
+            return current;
+        }else{
+            return max
+        }
+    })
+}
+console.log(math_max(33,-66,-1,12,76,45,10));
+
+
+
+//Destructuring --> Storing Values of arrays or object literals into mutiple variables;
+//On-ARRAYS
+let names = ["Mudasir","Ali","Owais","Rohan","Mesum"];
+let winner = names[0];
+let $2nd = names[1];
+let $3rd = names[2];
+//in order to reduce this work or easen this we use Destructuring.
+
+let [winnerr , secondWinner , ThirdWinner] = names; //it works on left to right sequence
+console.log(winner,$2nd,$3rd)
+
+let students = ["Mudasir Shaikh","Ahmed Ali","Owais Alam","Rohan Khan","Mesum Abbasi" ,"Abdul Baseer"];
+let [ first, second , ...others] = students;
+console.log(first, second , others);
+
+
+//ON OBJECT LITERALS;
+const student = {
+    name: "Muhammad Mudasir",
+    phone_no: 111222333,
+    city:"karachi",
+    dept : "CIS",
+    year:"2nd Year",
+    age:19,
+    username: "mudasir123",
+    password: "abcd123"
+}
+
+
+
+const { username , password} = student; //username and password keys are searched in the object and 
+                                        // then value will be assigned to them
+console.log(username , password);  
+
+
+//but if we want our own custom variable name which stores the key value then;
+const {username: user , password: secret , city} = student //new user will assigned to username value , same as for password
+console.log(user , secret ,city);
+
+const{ place:location="Block ABC NewCity" } = student; //we can also give defeault value incase the key is not found.\
 
 
 
